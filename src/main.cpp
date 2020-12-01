0f532a347f29b298c9c022311cf2f466f470cd91
EmilyEclipse <titdragos1@gmail.com>
2020-12-01 17:42:54 +0200

    Made paddle move horizonally, with no hiccups, with acceleration

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,14 @@ void safeSleep(const std::chrono::duration<int, std::milli>& timeFrameTookToRun,
     }
 }
 
+short calculateSpeed(short frameCount, short maxSpeed, short acceleration)
+{
+    short speed = frameCount * acceleration + acceleration;
+    if (speed <= maxSpeed)
+        return speed;
+    else return maxSpeed;
+}
+
 int main(int argc, char const *argv[])
 {
     const char* title = "BREAKOUT v0.1";
@@ -34,10 +42,15 @@ int main(int argc, char const *argv[])
     const bool variableFPS = false;
     bool gameRunning = true;
     SDL_Event event;
+    const Uint8* keyboardState = SDL_GetKeyboardState(NULL);
+    short movementSpeed = 0;
+    short maxMovementSpeed = 10;
+    short movementFrameCount = 0;
+    bool moving = false;
 
 
     
-    Paddle paddle(windowWidth / 2, windowHeight / 2, 300, 30);
+    Paddle paddle(windowWidth / 2, 900, 300, 50);
     Draw::setRenderer(window.getRenderer());
 
     //MAIN GAME LOOPS
@@ -50,36 +63,42 @@ int main(int argc, char const *argv[])
         {
             std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
 
-            SDL_SetRenderDrawColor(window.getRenderer(), 100, 100, 100, 255);
+            while(SDL_PollEvent(&event))
+            {
+                if(event.type == SDL_QUIT)
+                {
+                    gameRunning = false;
+                    break;
+                }
+            }
+
+            SDL_SetRenderDrawColor(window.getRenderer(), 0, 0, 0, 255);
             SDL_RenderClear(window.getRenderer());
 
             Draw::rect(paddle.getRect(), 255, 255, 255);
             SDL_RenderPresent(window.getRenderer());
-            
-            while(SDL_PollEvent(&event))
+
+            //keyboard input
             {
-                switch(event.type)
+                movementSpeed = calculateSpeed(movementFrameCount, maxMovementSpeed, 2);
+                if(keyboardState[SDL_SCANCODE_LEFT] || keyboardState[SDL_SCANCODE_A])
                 {
-                case SDL_QUIT:
-                    gameRunning = false;
-                    break;
-            
-                case SDL_KEYDOWN:
-                    switch(event.key.keysym.sym)
-                    {
-                    case SDLK_a:
-                    case SDLK_LEFT:
-                        paddle.moveLeft(10);
-                        break;
-
-                    case SDLK_d:
-                    case SDLK_RIGHT:
-                        paddle.moveRight(10);
-                    }
-                    break; 
+                    paddle.moveLeft(movementSpeed);
+                    moving = true;
                 }
-            }
 
+                if(keyboardState[SDL_SCANCODE_RIGHT] || keyboardState[SDL_SCANCODE_D])
+                {
+                    paddle.moveRight(movementSpeed);
+                    moving = true;
+                }
+
+                if(!keyboardState[SDL_SCANCODE_LEFT] && !keyboardState[SDL_SCANCODE_A] && !keyboardState[SDL_SCANCODE_RIGHT] && !keyboardState[SDL_SCANCODE_D])
+                    moving = false;
+                if(moving)
+                    ++movementFrameCount;
+                else  movementFrameCount = 0;
+            }
 
             std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<int, std::milli> timeFrameTookToRun = std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(end - start);
